0d896bf4a8caaf279e5ca2e70806bc9eafe65b06
vin <vinshentwianata@gmail,com>
2024-01-30 22:34:39 +0700

    mascotpaath

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -20,40 +20,36 @@ const getMascotImage = () => {
   return path;
 };
 
-const Layout: React.FC = ({ children, title = "This is the default title", className = ""}: Props) => {
-  const mascotImagePath = getMascotImage();
+const mascotPath = getMascotImage();
 
-  return (
-    <div>
-        <Head>
-          <link rel="icon" href={mascotImagePath} />
-          <title>{title}</title>
-          <meta charSet="utf-8" />
-          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-        </Head>
-        <header>
-          <nav>
-            <Link href="/">Menu</Link> | <Link href="/game">Start</Link> | <Link href="/about">About</Link> |{" "}
-          </nav>
-        </header>
-        <Image
-            src="/assets/bg-menu.svg"
-            alt="Canvas SVG"
-            width={200}
-            height={200}
-            className="background"
-          />
-        <div className={className}>
-        {children}
-        </div>
-        {/* <footer>
-          <hr />
-          <span>I'm here to stay (Footer)</span>
-        </footer> */}
-      </div>  
-    );
-};
+const Layout = ({ children, title = "This is the default title", className = ""}: Props) => (
+  <div>
+    <Head>
+      <link rel="icon" href={mascotPath} />
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <header>
+      <nav>
+        <Link href="/">Menu</Link> | <Link href="/game">Start</Link> | <Link href="/about">About</Link> |{" "}
+      </nav>
+    </header>
+    <Image
+        src="/assets/bg-menu.svg"
+        alt="Canvas SVG"
+        width={200}
+        height={200}
+        className="background"
+      />
+    <div className={className}>
+    {children}
+    </div>
+    {/* <footer>
+      <hr />
+      <span>I'm here to stay (Footer)</span>
+    </footer> */}
+  </div>
+);
 
 export default Layout;
-
-
